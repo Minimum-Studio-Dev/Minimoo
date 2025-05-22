2568b16a99e3a0adbf078a83384611043200b73c
ilseob lee <lysk9884@gmail.com>
2025-05-22 17:37:21 +0900

    카메라 resolutuion 추가

---
+++ Runtime/Camera/CameraResolution.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+
+namespace Minimoo.Camera
+{
+    [RequireComponent(typeof(Camera))]
+    [DisallowMultipleComponent]
+    public class CameraResolution : MonoBehaviour
+    {
+        public Camera targetCam;
+        public Vector2 Landscape = new Vector2(640, 360);
+
+        #region Unity Messages
+
+        protected virtual void Start()
+        {
+            if (targetCam == null)
+            {
+                targetCam = GetComponent<Camera>();
+            }
+
+            UpdateResolution();
+        }
+
+
+        #endregion
+
+        #region Public Methods
+        public void UpdateResolution()
+        {
+            var rect = targetCam.rect;
+
+            var targetSize = Landscape;
+
+            var scalewidth = (Screen.height / (float)Screen.width) / (targetSize.y / (float)targetSize.x); // (세로 / 가로)
+            var scaleheight = 1f / scalewidth;
+
+            if (scalewidth < 1)
+            {
+                rect.width = scalewidth;
+                rect.x = (1f - scalewidth) / 2f;
+            }
+            else
+            {
+                rect.height = scaleheight;
+                rect.y = (1f - scaleheight) / 2f;
+            }
+            targetCam.rect = rect;
+        }
+        #endregion
+
+    }
+}
